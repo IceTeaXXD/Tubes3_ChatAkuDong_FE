aef8d438209ace49419192b3c040d4ab78c40f09
henryanandsr <13521004@std.stei.itb.ac.id>
2023-05-04 00:34:00 +0700

    feat : add Home Page

---
+++ src/src/pages/HomePage.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import SideBar from "../components/SideBar";
+import History from "../components/History";
+import Chat from "../components/Chat";
+
+const HomePage = () => {
+    return(
+        <div>
+            <div className="bg-bg w-screen h-screen overflow-x-auto">
+                <div className="flex flex-row">
+                    <div className="w-1/12">
+                    <SideBar />
+                    </div>
+                    <div className="w-7/8">
+                    <History />
+                    </div>
+                    <div className="w-full bg-white">
+                    <Chat />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default HomePage;
