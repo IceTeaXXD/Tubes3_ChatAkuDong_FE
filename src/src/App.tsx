37e1b7596033494937784ffc8db3301475f190b6
henryanandsr <13521004@std.stei.itb.ac.id>
2023-05-05 19:28:17 +0700

    fix : refresh & cache

--- src/src/App.tsx
+++ src/src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import LoginRegister from './components/LoginRegister';
+import LoginRegister from './pages/LoginRegister';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ConversationPage from './pages/ConversationPage';
 import HomePage from './pages/HomePage';
@@ -7,16 +7,19 @@ import History from './components/History';
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [userID, setUserID] = useState(0);
-  const [convID, setConvID] = useState(0);
+  const [userID, setUserID] = useState(parseInt(localStorage.getItem('userID') || '0'));
+  const [convID, setConvID] = useState(parseInt(localStorage.getItem('convID') || '0'));
   const handleLogin = () => {
     setIsLoggedIn(true);
   };
   const handleSetUserID = (id: number | ((prevState: number) => number)) => {
     setUserID(id);
+    localStorage.setItem('userID', id.toString());
   };
+  
   const handleSetConvID = (id: number) => {
     setConvID(id);
+    localStorage.setItem('convID', id.toString());
   };
   return (
     <>
